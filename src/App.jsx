5cc529cd11ab7baf020b16c046b2a6a5c2523653
RosarioManny <rosario.emm47@gmail.com>
2024-10-25 16:49:50 -0400

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import './App.css'
+import viteLogo from '/vite.svg'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NavBar from './components/Partials/Navbar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar /> 
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/components/Partials/Navbar/NavBar.jsx
+++ src/components/Partials/Navbar/NavBar.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 const NavBar = () => {
     const user = false
 
-
     return (
         <>
         <Link to="/">TalkThroughIt</Link>
