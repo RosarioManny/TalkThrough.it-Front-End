a548cb29de4d19984f3b7bbc8c24631a81281c8f
RosarioManny <rosario.emm47@gmail.com>
2024-10-25 16:38:05 -0400

    Add Conditional

--- src/components/Partials/Navbar/NavBar.jsx
+++ src/components/Partials/Navbar/NavBar.jsx
@@ -1,10 +1,28 @@
-
-
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const NavBar = () => {
+    const user = false
+
 
     return (
-        <h1> I am Navbar</h1>
+        <>
+        <Link to="/">TalkThroughIt</Link>
+        {user ? (
+        <nav>
+            <Link to="provider">Find Provider</Link>
+            <Link to="message">Message</Link>
+            <Link to="signout">Sign-Out</Link>
+        </nav>
+        ) : (
+        <nav>
+            <Link to="provider">Find Provider</Link>
+            <Link to="message">Message</Link>
+            <Link to="signup">Sign-Up</Link>
+            <Link to="signiin">Sign-In</Link>
+        </nav>
+        )}
+        </>
     )
 };
 
