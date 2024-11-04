e29dbfc6bc8575287823c2311d765f2efa27ca03
Timothy Lim <contacttimothylim@gmail.com>
2024-11-04 15:28:17 -0500

    axios

--- src/services/appointmentService.js
+++ src/services/appointmentService.js
@@ -3,6 +3,7 @@ import { getAuthHeaders } from '../utils/auth';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.withCredentials = true;
 
 // Appointment Management
 export const createAppointment = async (appointmentData) => {
--- src/services/providerService.js
+++ src/services/providerService.js
@@ -3,6 +3,7 @@ import { getAuthHeaders } from '../utils/auth';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.withCredentials = true;
 
 // Fetch providers
 export const fetchProviders = async (params = {}) => {
